b4d07a156651769697045563f923aa400cd3a15e
❛ .𝁘ໍ𝛅 𝛕 ⋏ ᰻⃪᱂ 𐌺 𖡛̶ ̶ ̶᪳ ̶ <96779742+Ur-StaRk@users.noreply.github.com>
2025-04-04 17:26:45 +0530

    Update mustjoin.py

--- SHUKLAMUSIC/plugins/bot/mustjoin.py
+++ SHUKLAMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SHUKLAMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "aspirantDiscuss"
+MUST_JOIN = "sr7_blackbird"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
